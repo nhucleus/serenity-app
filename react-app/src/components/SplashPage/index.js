49e26e9e4f521c5b64c20f0fe27cd19a71e21db3
Nhu-y Phan <nhucleus@gmail.com>
2021-02-11 13:01:42 -0500

    chose color scheme

--- react-app/src/components/SplashPage/index.js
+++ react-app/src/components/SplashPage/index.js
@@ -1,10 +1,14 @@
 import React from "react";
+import "./SplashPage.css";
 
 function SplashPage() {
+  
   return (
-    <>
-      <h1>Splash Page TBD</h1>
-    </>
+    <div className="splash-container">
+      <div className="splash-content">
+        <h1>Splash Page TBD</h1>
+      </div>  
+    </div>
   )
 }
 
